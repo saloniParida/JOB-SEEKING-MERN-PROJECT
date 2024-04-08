7eb94e7c5277fd4be1b3a13cc568b47865429d6f
NOONECANSEE <anshusingla405@gmail.com>
2024-04-08 22:50:31 +0530

    FRONTEND

---
+++ FRONTEND/src/components/Layout/Footer.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+
+const Footer = () => {
+  const { isAuthorized } = useContext(Context);
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved @2024</div>
+      <div>
+        <Link to={"/"} >
+          <FaFacebookF />
+        </Link>
+        <Link to={"/"} >
+          <FaYoutube />
+        </Link>
+        <Link to={"/"} >
+          <FaLinkedin />
+        </Link>
+        <Link to={"/"} >
+          <RiInstagramFill />
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
